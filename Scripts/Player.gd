c561a9e30470404af3a3fb5bdf4e9ef9cfa6a74b
Matthew Hillier <26564949+MHillier98@users.noreply.github.com>
2020-04-18 17:31:04 +1000

    Got head animation working

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -4,6 +4,8 @@ export (int) var speed = 400
 
 var velocity = Vector2()
 
+onready var animationPlayer = $AnimationPlayer
+
 func get_input():
 	velocity = Vector2()
 	if Input.is_action_pressed('right'):
@@ -19,3 +21,12 @@ func get_input():
 func _physics_process(delta):
 	get_input()
 	velocity = move_and_slide(velocity)
+	
+	if get_global_mouse_position().x < global_position.x:
+		animationPlayer.play("Look Left")
+		get_node("HeadCollisionShape/HeadSprite").set_flip_v(true)
+		yield(animationPlayer, "animation_finished")
+	else:
+		animationPlayer.play("Look Right")
+		get_node("HeadCollisionShape/HeadSprite").set_flip_v(false)
+		yield(animationPlayer, "animation_finished")
--- Scripts/PlayerHead.gd
+++ Scripts/PlayerHead.gd
@@ -1,36 +1,12 @@
-extends KinematicBody2D
+extends CollisionShape2D
 
-export (int) var speed = 20
+#export (int) var speed = 20
 
 func get_input():
-	look_at(get_global_mouse_position())
+	var mouse_pos = get_global_mouse_position()
+	
+	if (mouse_pos.x > global_position.x + 20 or mouse_pos.x < global_position.x - 20) and global_position.distance_to(mouse_pos) > 10:
+		look_at(mouse_pos)
 
 func _physics_process(delta):
-	pass
-	# this isn't working
-	
 	get_input()
-	var mouse_pos = get_global_mouse_position()
-#	if mouse_pos.x != 0 and mouse_pos.y != 0:
-
-	print("---")
-#	print(mouse_pos.x)
-	print(global_position)
-	
-	var player_pos = get_parent().global_position
-	print(player_pos)
-	
-	var target = Vector2(0, 0)
-	
-	if mouse_pos.x > global_position.x:
-		target = Vector2(player_pos.x + 10, player_pos.y - 55)
-#		var velocity = position.direction_to(target) * speed # position.move_toward(, 55)
-	else:
-		target = Vector2(player_pos.x - 10, player_pos.y - 55)
-#		var velocity = position.direction_to(target) * speed # position.move_toward(Vector2(player_pos.x - 26, player_pos.y + 20), 55)
-	
-	print(target.x)
-	
-	var velocity = global_position.direction_to(target) * speed
-	if global_position.distance_to(target) > 20:
-		velocity = move_and_slide(velocity)
